0a58f8ff7e71047d42fb336af2eab20c984ad3bb
chuacn1 <chuacn1@student.op.ac.nz>
2025-11-06 16:34:14 +1300

    added console.log to debug

--- backend/prisma/client.js
+++ backend/prisma/client.js
@@ -2,4 +2,14 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient();
 
+async function testDB() {
+    try {
+      await prisma.$connect()
+      console.log('✅ Database connected successfully')
+    } catch (error) {
+      console.error('❌ Database connection failed:', error)
+    }
+  }
+  testDB()
+
 export default prisma
